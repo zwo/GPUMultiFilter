a220db278a251872b0f39706b2b97c219382ba0e
zhouweiou <zhouweiou@rfchina.com>
2018-04-04 16:22:13 +0800

    new parameters

--- GPUMultiFIlter/Engine/FZInkSim.m
+++ GPUMultiFIlter/Engine/FZInkSim.m
@@ -82,28 +82,52 @@
 
 void restoreToSystemDefaults(FZUniformInfos *infos)
 {
-    infos->brushsize = 7.086956501;
-    infos->baseMask = 0.037267081;
-    infos->gamma = 0.037267081;
-    infos->omega = 0.968944073;
-    infos->advect_p = 0.100000001;
-    infos->evapor_b = 0.000010000;
-    infos->evapor = 0.000500000;
-    infos->b11 = 0.009316770;
-    infos->b12 = 0.391304344;
-    infos->b13 = 0.009316770;
-    infos->b21 = 0.123152710;
-    infos->b22 = 0.307453424;
-    infos->p1 = 0.000000000;
-    infos->p2 = 0.300000012;
-    infos->p3 = 0.200000003;
-    infos->ba1 = 0.000040994;
-    infos->ba2 = 0.000043168;
-    infos->f1 = 0.010000000;
-    infos->f2 = 0.090000004;
-    infos->f3 = 0.090000004;
-    infos->toe_p = 0.100000001;
-    infos->waterAmount = 1.000000000;
+//    infos->brushsize = 7.086956501;
+//    infos->baseMask = 0.037267081;
+//    infos->gamma = 0.037267081;
+//    infos->omega = 0.968944073;
+//    infos->advect_p = 0.100000001;
+//    infos->evapor_b = 0.000010000;
+//    infos->evapor = 0.000500000;
+//    infos->b11 = 0.009316770;
+//    infos->b12 = 0.391304344;
+//    infos->b13 = 0.009316770;
+//    infos->b21 = 0.123152710;
+//    infos->b22 = 0.307453424;
+//    infos->p1 = 0.000000000;
+//    infos->p2 = 0.300000012;
+//    infos->p3 = 0.200000003;
+//    infos->ba1 = 0.000040994;
+//    infos->ba2 = 0.000043168;
+//    infos->f1 = 0.010000000;
+//    infos->f2 = 0.090000004;
+//    infos->f3 = 0.090000004;
+//    infos->toe_p = 0.100000001;
+//    infos->waterAmount = 1.000000000;
+//    infos->wf_mul = 1.0f;
+    
+    infos->brushsize=7.08696;
+    infos->waterAmount=0.966135;
+    infos->gamma=0.961155;
+    infos->baseMask=0.961155;
+    infos->toe_p=0.00398406;
+    infos->b11=0.0059761;
+    infos->b12=0.0209163;
+    infos->b13=0.00931677;
+    infos->b21=0.0219124;
+    infos->b22=0.0268924;
+    infos->p1=0;
+    infos->p2=0.3;
+    infos->p3=0.2;
+    infos->omega=0.968944;
+    infos->advect_p=0.1;
+    infos->evapor=0.0005;
+    infos->evapor_b=1e-05;
+    infos->ba1=4.0994e-05;
+    infos->ba2=0.0239044;
+    infos->f1=0.01;
+    infos->f2=0.09;
+    infos->f3=0.09;
     infos->wf_mul = 1.0f;
 }
 
@@ -179,6 +203,7 @@ void restoreToSystemDefaults(FZUniformInfos *infos)
 //            NSLog(@"count %zd",_debugCounter);
             if (_debugCounter==2) {
                 NSLog(@"hit");
+//                [self.fboDepositionBuffer saveImageToDocument];
             }
         }
         
